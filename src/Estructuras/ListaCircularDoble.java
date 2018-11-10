ca97dab5fc4d57aca1daca3c98f78e08c00f7f26
carlosmz87 <carlosmartz1995@gmail.com>
2018-11-09 21:28:34 -0600

    Commit 2

--- src/Estructuras/ListaCircularDoble.java
+++ src/Estructuras/ListaCircularDoble.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Estructuras;
+import Control.Estudiantes;
 import Nodos.NodoEstudiantes;
 
 /**
@@ -23,7 +24,57 @@ public class ListaCircularDoble {
     }
     
     //CREA UN NUEVO ESTUDIANTE Y LO INGRESA EN LA LISTA
-    public void IngresarEstudiante(int x){
+    public void IngresarEstudiante(Estudiantes x){
+        //CREAMOS UN NUEVO NODO
+        NodoEstudiantes nuevo = new NodoEstudiantes();
+        
+        nuevo.dato = x;
+        
+        //SI EL PRIMER DATO APUNTA A NULO
+        if(primero == null){
+            //EL APUNTADOR PRIMERO APUNTA AL NUEVO ESTUDIANTE
+            primero = nuevo;
+            //EL APUNTADOR SIGUIENTE DEL NUEVO NODO APUNTA AL PRIMERO DEL NUEVO NODO
+            primero.siguiente = primero;
+            //EL APUNTADOR ANTERIOR DEL NUEVO NODO APUNTA A ULTIMO
+            nuevo.anterior = ultimo;
+            //EL APUNTADOR ULTIMO APUNTA AL NUEVO NODO 
+            ultimo = nuevo;
+        }
+        
+        //SI LA LISTA NO ESTA VACIA
+        else{
+            //EL APUNTADOR ULTIMO DE LA LISTA APUNTA COMO SIGUIENTE AL NUEVO NODO
+            ultimo.siguiente = nuevo;
+            //EL APUNTADOR SIGUIENTE DEL NUEVO NODO APUNTA AL PRIMERO DE LA LISTA
+            nuevo.siguiente = primero;
+            //EL APUNTADOR ANTERIOR DEL NUEVO NODO APUNTA AL ULTIMO
+            nuevo.anterior = ultimo;
+            //EL APUNTADOR ULTIMO APUNTA AL NUEVO NODO
+            ultimo = nuevo;
+            //EL APUNTADOR ANTERIOR APUNTA AL ULTIMO DE LA LISTA
+            primero.anterior = ultimo;
+        }
+    }
+    
+    //LA FUNCION MOSTRAR DESPLOEGO LOS ESTUDIANTES ALMACENADOS
+    public void Mostrar(){
+        //SE CREA EL NUEVO NODO AUXILIAR
+        NodoEstudiantes actual = new NodoEstudiantes();
+        //EL NODO ACTUAL APUNTA AL PRIMERO DE LA LISTA
+        actual = primero;
+        //SE CREA EL CICLO DO WHILE
+        do{
+            //IMPRIME LOS DATOS DEL ESTUDIANTE
+            System.out.println(actual.dato);
+            //ACTUAL PASA AL SIGUIENTE NODO
+            actual = actual.siguiente;
+        }
+        //MIENTRAS NO APUNTE AL PRIMER ESTUDIANTE SE REALIZA EL CICLO DO WHILE
+        while(actual != primero);
         
     }
+    
+    
+    
 }
